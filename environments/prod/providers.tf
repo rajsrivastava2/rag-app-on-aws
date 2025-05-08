9c9f5848c8d5cbb0e1b26c02160cdb7399a9ee8e
Rajesh <genieincodebottle@gmail.com>
2025-05-08 14:32:58 +0530

    Added missing providers terraform code

--- environments/prod/providers.tf
+++ environments/prod/providers.tf
@@ -1,15 +1,5 @@
 provider "aws" {
   region = var.aws_region
-  
-  default_tags {
-    tags = {
-      Project     = var.project_name
-      Environment = var.stage
-      ManagedBy   = "Terraform"
-      Application = "RAG App on AWS"
-      CreatedAt   = formatdate("YYYY-MM-DD", timestamp())
-    }
-  }
 }
 
 # Required provider versions for better stability
